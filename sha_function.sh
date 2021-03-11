15733b219755e41f1104ed2cf1842032de0c98a4
root <root@travis-job-91937532-d593-4613-b139-18e218cd436b>
2021-03-11 04:52:11 +0000

    updates alpine

--- sha_function.sh
+++ sha_function.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-get_variant_sha(){
+# get_manifest_sha function is going to get alpine docker image's sha of all kinds of archtecture: amd64 armv6 armv7 armv8 arm64 386 etc
+get_manifest_sha(){
     local sha
-    docker_repo=$1  #alpine or vmnet/alpine
-    manifest_tag=$2
+    docker_repo=$1  #alpine or neethumohan1212/alpine
+    manifest_tag=$2  #latest or any tag name
     docker_image=$docker_repo:$manifest_tag
-    arch=$3
+    arch=$3         # amd64 or arm or arm64
     variant=$4
     export DOCKER_CLI_EXPERIMENTAL=enabled
 
@@ -30,9 +30,9 @@ get_variant_sha(){
         i=$i+1
     done < "$2".txt
 }
-
-get_manifest_sha (){
-    local repo=$1
+# get_simple_manifest_sha is going to get neethumohan1212/alpine docker image's sha, three archectures: amd64 and arm arm64
+get_simple_manifest_sha (){
+    local repo=$1  # my repo name simple/alpine:latest
     local arch=$2
     docker pull -q $1 &>/dev/null
     docker manifest inspect $1 > "$2".txt
@@ -49,9 +49,9 @@ get_manifest_sha (){
     done < "$2".txt
 
 }
-
+# get_tag_sha function is going to get balenalib:raspberry-pi-alpine image's sha.
 get_tag_sha(){
-    local repo=$1
+    local repo=$1    # this repo is not a manifest docker image
     local tag=$2
     docker pull "$repo:$tag" &>/dev/null
     sha=$(docker inspect --format='{{index .RepoDigests 0}}' "$repo:$tag" 2>/dev/null | cut -d @ -f 2)
@@ -69,13 +69,14 @@ compare_sha () {
 create_manifests(){
     local repo=$1
     local tag=$2
-    local x86=$3
-    local rpi=$4
-    local arm64=$5
-    docker manifest create $repo:$tag $x86 $rpi $arm64
-    docker manifest create $repo:latest $x86 $rpi $arm64
-    docker manifest annotate $repo:latest $rpi --arch arm
-    docker manifest annotate $repo:$tag $arm64 --arch arm64
-    docker manifest annotate $repo:latest $arm64 --arch arm64
+    local tag2=$3
+    local x86=$4
+    local rpi=$5
+    local arm64=$6
+    docker manifest create $repo:$tag $x86 $rpi $arm64    # there are three archtecture in this manifest repo
+    docker manifest create $repo:$tag2 $x86 $rpi $arm64
     docker manifest annotate $repo:$tag $rpi --arch arm
+    docker manifest annotate $repo:$tag2 $arm64 --arch arm64
+    docker manifest annotate $repo:$tag $arm64 --arch arm64
+    docker manifest annotate $repo:$tag2 $rpi --arch arm
 }
