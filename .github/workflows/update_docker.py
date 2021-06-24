f3f8e58688b816b1f112e5c86583e4d1a1e25994
Derek Yeh <djyeh@uci.edu>
2021-06-23 20:33:41 -0700

    Update update_docker.py

--- .github/workflows/update_docker.py
+++ .github/workflows/update_docker.py
@@ -46,8 +46,11 @@ description = driver.find_element_by_class_name("styles__contents___2GAXQ")
 
 description.clear()
 description.send_keys(getReadMe())
+driver.implicitly_wait(35)
+
 update_button = driver.find_element_by_class_name("dbutton.styles__button___349c4.styles__new___28c7_")
 
+driver.implicitly_wait(35)
 update_button.click()
 
 print("Updated description...")
