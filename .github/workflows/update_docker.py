80313f4c46f1374accf003d9f2e93dcc2f5715b9
Derek Yeh <djyeh@uci.edu>
2021-06-23 21:01:24 -0700

    Update update_docker.py

--- .github/workflows/update_docker.py
+++ .github/workflows/update_docker.py
@@ -10,8 +10,16 @@ def getReadMe():
     return txt
   
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--proxy-server='direct://'")
+chrome_options.add_argument("--proxy-bypass-list=*")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--ignore-certificate-errors')
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.maximize_window()
 
