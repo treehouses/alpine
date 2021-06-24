6f83d7c544faedaef1f96646457a21e99aa44461
Derek Yeh <djyeh@uci.edu>
2021-06-23 21:17:03 -0700

    Update update_docker.py

--- .github/workflows/update_docker.py
+++ .github/workflows/update_docker.py
@@ -20,6 +20,9 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--allow-insecure-localhost')
+chrome_options.add_argument('--allow-running-insecure-content')
+
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.maximize_window()
 
