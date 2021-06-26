047862495971ffa1a30ac64c668d279ef1909742
Derek Yeh <djyeh@uci.edu>
2021-06-25 21:35:12 -0700

    add options

--- .github/workflows/update_docker.py
+++ .github/workflows/update_docker.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 def getReadMe():
     with open(sys.argv[3], 'r') as f:
